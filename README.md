ad55e25be04c0836f284c460cb07042596371b20
lisang <ranchangxin@gmail.com>
2023-10-10 21:04:08 +0800

    init

---
+++ README.md
@@ -0,0 +1,12 @@
+# 校园论坛小程序后端
+
+> 使用技术栈
+
+| --                  | --                |
+| ------------------- | ----------------- |
+| 后端框架            | SpringBoot 3.1.0  |
+| 数据校验            | Spring-Validation |
+| 接口文档            | SpringDoc 2.1.0   |
+| Token和用户角色校验 | Jwt               |
+| 数据库              | Postgresql        |
+| 持久层框架          | Myabtis-flex      |
---
+++ src/main/java/com/zeroxn/bbs/ForumApplication.java
@@ -0,0 +1,16 @@
+package com.zeroxn.bbs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Author: lisang
+ * @DateTime: 2023-10-10 20:48:07
+ * @Description: 论坛程序启动类
+ */
+@SpringBootApplication
+public class ForumApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ForumApplication.class, args);
+    }
+}
